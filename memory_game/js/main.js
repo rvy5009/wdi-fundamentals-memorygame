fc208117f59a0d9027ed742510915808ebf778a3
Raymond Yen <rvy5009@Raymonds-MacBook-Pro.local>
2019-11-02 19:48:57 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,47 @@
 
 
 
-var cards = ["queen", "queen", "king", "king"]
+var cards = [
+	{ 	rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{ 	rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{ 	rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{ 	rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
+	]
 var cardsInPlay = []
 
-var cardOne = cards[0]
-cardsInPlay.push(cardOne)
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2]
-cardsInPlay.push(cardTwo)
-console.log("User flipped " + cardTwo);
-
-if( cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match");
-	}
-	else{
-		alert("Sorry try again")
+function checkForMatch(){
+	if( cardsInPlay.length === 2){
+		if(cardsInPlay[0] === cardsInPlay[1]){
+			alert("You found a match");
+		}
+		else{
+			alert("Sorry try again")
+		}
 	}
 }
+
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId].rank +cards[cardId].suit + cards[cardId].cardImage)
+
+	cardsInPlay.push(cards[cardId].rank)
+
+	checkForMatch()
+}
+flipCard(0)
+flipCard(2)
+
+
+
+
